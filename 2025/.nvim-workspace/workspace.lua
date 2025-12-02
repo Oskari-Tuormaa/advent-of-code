9bcd72b14d73349a414211e2523e3765414cefb6
Oskari Kristian Tuormaa <oskaritu@gmail.com>
2025-12-02 15:53:36 +0100

    I prefer Ninja

--- 2025/.nvim-workspace/workspace.lua
+++ 2025/.nvim-workspace/workspace.lua
@@ -1,12 +1,12 @@
 local nmap = require("config.utils").nmap
 
-nmap("<leader>e", "<cmd>sp term://cmake --build build<cr>")
-nmap("<leader>bp", "<cmd>sp term://cmake -B build<cr>")
+nmap("<leader>e", "<cmd>sp term://cmake --build build -j<cr>")
+nmap("<leader>bp", "<cmd>sp term://cmake --fresh -B build -G Ninja<cr>")
 
 nmap("<leader>E", function()
     vim.notify("Here we go")
     local days = vim.fn.glob("day*", true, true)
-    local cmd = "sp term://cmake --build build"
+    local cmd = "sp term://cmake --build build -j"
 
     for _, k in pairs(days) do
         cmd = cmd .. " && echo && build/" .. k ..'/'.. k
@@ -22,5 +22,5 @@ nmap("<leader>W", function()
     local day = string.format("day%02d", day_idx)
     local execute_cmd = "build/".. day .."/".. day
 
-    vim.cmd([[ sp term://bash -c 'fd \| entr -cs \"cmake --build build && ]].. execute_cmd ..[[\"' ]])
+    vim.cmd([[ belowright vs term://bash -c 'fd \| entr -cs \"cmake --build build -j && ]].. execute_cmd ..[[\"' ]])
 end)
